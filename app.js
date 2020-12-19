abbff10caeb40640d6889688d0cdcc35725a0842
Freddy Caron <freddycaron.1@gmail.com>
2020-12-19 15:29:18 +0100

    création des utilisateurs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Frc-web:Todayfred30@cluster0.lcfyu.mongodb.net/be-fullstack?retryWrites=true&w=majority',
 {
@@ -30,5 +31,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
